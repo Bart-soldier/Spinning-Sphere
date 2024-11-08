c02c943680d645ca327950f5cc134a797fbd7b87
Billy C. <clercbilly@gmail.com>
2024-11-08 14:56:27 +0100

    Initial cleanup

---
+++ Spinning Sphere/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody Rigidbody;
+
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
