e4a2fc365b8a606ef7f44f7f893ac75cef3abce3
Billy C. <clercbilly@gmail.com>
2024-11-09 10:00:11 +0100

    Added dissolve animation

---
+++ Spinning Sphere/Assets/Scripts/DissolveAnimator.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class DissolveAnimator : MonoBehaviour
+{
+    public bool StartAnimation = false;
+    public float AnimationSpeed = 1.5f;
+
+    private bool IsAnimating = false;
+    private bool Appearing = false;
+
+    void Update()
+    {
+        if(StartAnimation)
+        {
+            if(IsAnimating)
+            {
+                Appearing = !Appearing;
+            }
+            else
+            {
+                Appearing = this.GetComponent<Renderer>().material.GetFloat("_Animation") >= 0.5f;
+            }
+
+            IsAnimating = true;
+            StartAnimation = false;
+        }
+
+        if(IsAnimating)
+        {
+            float animation = this.GetComponent<Renderer>().material.GetFloat("_Animation");
+            animation = Appearing ? animation - AnimationSpeed * Time.deltaTime : animation + AnimationSpeed * Time.deltaTime;
+
+            IsAnimating = animation > 0.0f && animation < 1.0f;
+
+            this.GetComponent<Renderer>().material.SetFloat("_Animation", animation);
+        }
+    }
+}
